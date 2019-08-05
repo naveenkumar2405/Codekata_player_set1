a2e8ecbca4f83ca680f6a922c3d1652014090277
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-06 00:54:31 +0530

    Update CKP7.cpp

--- CKP7.cpp
+++ CKP7.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main()
 {
      char str[20],tmp;
-     int i,j;
      cin>>str;
-     for(i=0;i<strlen(str);i=i+2){
+     for(int i=0 ; i<strlen(str) ; i=i+2){
      tmp = str[i];
      str[i] = str[i+1];
      str[i+1] = tmp;
